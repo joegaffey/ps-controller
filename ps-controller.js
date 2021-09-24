aa92104f7c0cace4365e1f1fd0cc71a06e0780c6
Glitch (ps-controller-staging) <none>
2021-09-24 10:05:00 +0000

    Added pointer interaction

--- ps-controller.js
+++ ps-controller.js
@@ -4,34 +4,64 @@ import { DualShock4 } from './webhid-ds4.js';
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
-  * { font-family: Arial, Helvetica, sans-serif; }
+  .vector.text {
+    user-select: none;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 24pt;      
+    fill: #000000;
+    text-anchor: middle; 
+  }
+  line, ellipse { 
+    stroke: #000000;
+    stroke-width: 2px;
+  } 
+  .force {
+    stroke-width: 12px;
+    stroke: #ff0000;
+    stroke-linecap: round;
+  }
   .hidden { visibility: hidden; }
 </style>
-<object onload="ds4svg = this;" id="ds4svg" type="image/svg+xml" data="ds4.svg"></object>
+<object id="ds4svg" type="image/svg+xml" data="ds4.svg"></object>
 <div class="hidden">
-  <object onload="gyroSvg = this;" id="gyro_svg" type="image/svg+xml" data="vectors.svg"></object>
-  <object onload="accelSvg = this;" id="accel_svg" type="image/svg+xml" data="vectors.svg"></object>
+  <object id="gyro_svg" class="vector" type="image/svg+xml" data="vectors.svg"></object>
+  <object id="accel_svg" class="vector" type="image/svg+xml" data="vectors.svg"></object>
 </div>   
 `;
 
-class PSController extends HTMLElement {
-  
+class PSController extends HTMLElement {  
+
   constructor() {
     super();
-    let shadowRoot = this.attachShadow({mode: 'open'});
+    
+    const shadowRoot = this.attachShadow({mode: 'open'});
     shadowRoot.appendChild(template.content.cloneNode(true));
-  }
-  
-  async init () {
-    this.ds4svg = this.shadowRoot.querySelector('#ds4svg'); 
+        
+    this.ds4svg = this.shadowRoot.querySelector('#ds4svg');     
     this.gyroSvg = this.shadowRoot.querySelector('#gyro_svg'); ;
     this.accelSvg = this.shadowRoot.querySelector('#accel_svg'); 
-    this.setupSVGs();
     
+    const loadSVGPromises = [
+      new Promise((resolve) => { this.gyroSvg.onload = resolve; }),
+      new Promise((resolve) => { this.ds4svg.onload = resolve; }),
+      new Promise((resolve) => { this.accelSvg.onload = resolve; })
+    ];
+    Promise.all(loadSVGPromises).then(() => { this.setupSVGs(); });
+    
+    this.userState = this.getIinitalState();
+    this.updateController();
+  }
+  
+  setupSVGs() {
+    this.setupDS4SVG();
+    this.setupGyroSVG();
+    this.setupAccelSVG();
+  }
+  
+  async init () {   
     this.DS4 = new DualShock4();  
     // This will request the WebHID device and initialize the controller
-    await this.DS4.init();    
-    this.updateController();
+    await this.DS4.init();
   }
   
   updateController() {
@@ -61,33 +91,37 @@ class PSController extends HTMLElement {
   }
   
   update() {
-    if(!this.DS4)
+    if(!this.ds4svg.isSetUp) 
       return;
-    this.DS4.state.buttons.square ? this.squareShape.style.stroke = 'red' : this.squareShape.style.stroke = 'black';
-    this.DS4.state.buttons.circle ? this.circleShape.style.stroke = 'red' : this.circleShape.style.stroke = 'black';
-    this.DS4.state.buttons.cross ? this.crossShape1.style.stroke = 'red' : this.crossShape1.style.stroke = 'black';
-    this.DS4.state.buttons.cross ? this.crossShape2.style.stroke = 'red' : this.crossShape2.style.stroke = 'black';
-    this.DS4.state.buttons.triangle ? this.triangleShape.style.stroke = 'red' : this.triangleShape.style.stroke = 'black';
     
-    this.DS4.state.buttons.dPadLeft ? this.dpadLeftShape.style.fill = 'red' : this.dpadLeftShape.style.fill = '';
-    this.DS4.state.buttons.dPadRight ? this.dpadRightShape.style.fill = 'red' : this.dpadRightShape.style.fill = '';
-    this.DS4.state.buttons.dPadUp ? this.dpadUpShape.style.fill = 'red' : this.dpadUpShape.style.fill = '';
-    this.DS4.state.buttons.dPadDown ? this.dpadDownShape.style.fill = 'red' : this.dpadDownShape.style.fill = '';
+    let state = this.userState;
+    if(this.DS4)
+      state = this.DS4.state;
     
-    this.DS4.state.buttons.l1 ? this.l1Shape.style.fill = 'red' : this.l1Shape.style.fill = '';
-    this.DS4.state.buttons.r1 ? this.r1Shape.style.fill = 'red' : this.r1Shape.style.fill = '';
+    state.buttons.square ? this.squareShape.style.stroke = 'red' : this.squareShape.style.stroke = 'black';
+    state.buttons.circle ? this.circleShape.style.stroke = 'red' : this.circleShape.style.stroke = 'black';
+    state.buttons.cross ? this.crossShape.style.stroke = 'red' : this.crossShape.style.stroke = 'black';
+    state.buttons.triangle ? this.triangleShape.style.stroke = 'red' : this.triangleShape.style.stroke = 'black';
     
-    this.DS4.state.buttons.l3 ? this.l3Shape.style.stroke = 'red' : this.l3Shape.style.stroke = 'black';
-    this.DS4.state.buttons.r3 ? this.r3Shape.style.stroke = 'red' : this.r3Shape.style.stroke = 'black';
+    state.buttons.dPadLeft ? this.dpadLeftShape.style.fill = 'red' : this.dpadLeftShape.style.fill = '';
+    state.buttons.dPadRight ? this.dpadRightShape.style.fill = 'red' : this.dpadRightShape.style.fill = '';
+    state.buttons.dPadUp ? this.dpadUpShape.style.fill = 'red' : this.dpadUpShape.style.fill = '';
+    state.buttons.dPadDown ? this.dpadDownShape.style.fill = 'red' : this.dpadDownShape.style.fill = '';
     
-    this.DS4.state.buttons.share ? this.shareShape.style.fill = 'red' : this.shareShape.style.fill = '';
-    this.DS4.state.buttons.options ? this.optionsShape.style.fill = 'red' : this.optionsShape.style.fill = '';
+    state.buttons.l1 ? this.l1Shape.style.fill = 'red' : this.l1Shape.style.fill = '';
+    state.buttons.r1 ? this.r1Shape.style.fill = 'red' : this.r1Shape.style.fill = '';
     
-    this.DS4.state.buttons.touchPadClick ? this.touchpadShape.style.stroke = 'red' : this.touchpadShape.style.stroke = '';    
-    this.DS4.state.buttons.playStation ? this.playstationShape.style.fill = 'red' : this.playstationShape.style.fill = '';
+    state.buttons.l3 ? this.l3Shape.style.stroke = 'red' : this.l3Shape.style.stroke = 'black';
+    state.buttons.r3 ? this.r3Shape.style.stroke = 'red' : this.r3Shape.style.stroke = 'black';
     
-    if(this.DS4.state.touchpad.touches && this.DS4.state.touchpad.touches.length > 0) {
-      this.DS4.state.touchpad.touches.forEach((touch, i) => {
+    state.buttons.share ? this.shareShape.style.fill = 'red' : this.shareShape.style.fill = '';
+    state.buttons.options ? this.optionsShape.style.fill = 'red' : this.optionsShape.style.fill = '';
+    
+    state.buttons.touchPadClick ? this.touchpadShape.style.stroke = 'red' : this.touchpadShape.style.stroke = '';    
+    state.buttons.playStation ? this.playstationShape.style.fill = 'red' : this.playstationShape.style.fill = '';
+    
+    if(state.touchpad.touches && state.touchpad.touches.length > 0) {
+      state.touchpad.touches.forEach((touch, i) => {
         if(i === 0) {
           this.touch1.style.fill = 'red';
           this.touch1.setAttribute('cx', 205 + touch.x / 10); 
@@ -105,82 +139,79 @@ class PSController extends HTMLElement {
       this.touch2.style.fill = 'none';
     }
     
-    if(this.DS4.state.axes.rightStickX > 138 || this.DS4.state.axes.rightStickX < 118 || this.DS4.state.axes.rightStickY > 138 || this.DS4.state.axes.rightStickY < 118) {
+    if(state.axes.rightStickX > 138 || state.axes.rightStickX < 118 || state.axes.rightStickY > 138 || state.axes.rightStickY < 118) {
       this.rightStickShape.style.fill = 'red'
-      this.rightStickShape.setAttribute('transform', `translate( ${(this.DS4.state.axes.rightStickX - 128) / 10} , ${(this.DS4.state.axes.rightStickY - 128) / 10} )`);
+      this.rightStickShape.setAttribute('transform', `translate( ${(state.axes.rightStickX - 128) / 10} , ${(state.axes.rightStickY - 128) / 10} )`);
     }
     else {
       this.rightStickShape.style.fill = ''
       this.rightStickShape.setAttribute('transform', `translate(0, 0)`);
     }
     
-    if(this.DS4.state.axes.leftStickX > 138 || this.DS4.state.axes.leftStickX < 118 || this.DS4.state.axes.leftStickY > 138 || this.DS4.state.axes.leftStickY < 118) {
+    if(state.axes.leftStickX > 138 || state.axes.leftStickX < 118 || state.axes.leftStickY > 138 || state.axes.leftStickY < 118) {
       this.leftStickShape.style.fill = 'red'
-      this.leftStickShape.setAttribute('transform', `translate( ${(this.DS4.state.axes.leftStickX - 128) / 10} , ${(this.DS4.state.axes.leftStickY - 128) / 10} )`);
+      this.leftStickShape.setAttribute('transform', `translate( ${(state.axes.leftStickX - 128) / 10} , ${(state.axes.leftStickY - 128) / 10} )`);
     }
     else {
       this.leftStickShape.style.fill = ''
       this.leftStickShape.setAttribute('transform', `translate(0, 0)`);
     }
     
-    if(this.DS4.state.axes.l2 > 5) {
+    if(state.axes.l2 > 5) {
       this.lTrigger.style.fill = 'red' 
-      this.lTrigger.setAttribute('r', this.DS4.state.axes.l2 / 5); 
+      this.lTrigger.setAttribute('r', state.axes.l2 / 5); 
     } 
     else
       this.lTrigger.style.fill = 'none';
     
-    if(this.DS4.state.axes.r2 > 5) {
+    if(state.axes.r2 > 5) {
       this.rTrigger.style.fill = 'red' 
-      this.rTrigger.setAttribute('r', this.DS4.state.axes.r2 / 5); 
+      this.rTrigger.setAttribute('r', state.axes.r2 / 5); 
     } 
     else
       this.rTrigger.style.fill = 'none';
     
-    this.gxLine.setAttribute('y2', (65535 - this.DS4.state.axes.gyroX) / 1000 | 0);
-    this.gyLine.setAttribute('y2', (65535 - this.DS4.state.axes.gyroY) / 1000 | 0);
-    this.gzLine.setAttribute('y2', (65535 - this.DS4.state.axes.gyroZ) / 1000 | 0);
+    if(this.gyroSvg.isSetUp) { 
+      this.gxLine.setAttribute('y2', (65535 - state.axes.gyroX) / 1000 | 0);
+      this.gyLine.setAttribute('y2', (65535 - state.axes.gyroY) / 1000 | 0);
+      this.gzLine.setAttribute('y2', (65535 - state.axes.gyroZ) / 1000 | 0);
+    }
     
-    this.axLine.setAttribute('y2', this.DS4.state.axes.accelX / -500 | 0);
-    this.ayLine.setAttribute('y2', this.DS4.state.axes.accelY / -500 | 0);
-    this.azLine.setAttribute('y2', this.DS4.state.axes.accelZ / -500 | 0);
+    if(this.accelSvg.isSetUp) { 
+      this.axLine.setAttribute('y2', state.axes.accelX / -500 | 0);
+      this.ayLine.setAttribute('y2', state.axes.accelY / -500 | 0);
+      this.azLine.setAttribute('y2', state.axes.accelZ / -500 | 0);
+    }
   }  
-  
-  setupSVGs() {
-    this.setupDS4SVG();
-    this.setupGyroSVG();
-    this.setupAccelSVG();
-  }
 
   setupDS4SVG() {
     const svgDoc = this.ds4svg.contentDocument;
     svgDoc.getElementById('svg6306').setAttributeNS(null, 'height', 430);
 
-    this.touchpadShape = svgDoc.getElementById('rect3842');
-    this.playstationShape = svgDoc.getElementById('path3840');
+    this.makeButtonInteractive(this.touchpadShape = svgDoc.getElementById('rect3842'), 'touchpad');
+    this.makeButtonInteractive(this.playstationShape = svgDoc.getElementById('path3840'), 'playstation');
 
-    this.squareShape = svgDoc.getElementById('rect4554');
-    this.triangleShape = svgDoc.getElementById('rect4554-1');
-    this.circleShape = svgDoc.getElementById('path4552');
-    this.crossShape1 = svgDoc.getElementById('path4532');
-    this.crossShape2 = svgDoc.getElementById('path4532-0');
+    this.makeButtonInteractive(this.squareShape = svgDoc.getElementById('rect4554'), 'square');
+    this.makeButtonInteractive(this.triangleShape = svgDoc.getElementById('rect4554-1'), 'triangle');
+    this.makeButtonInteractive(this.circleShape = svgDoc.getElementById('path4552'), 'circle');
+    this.makeButtonInteractive(this.crossShape = svgDoc.getElementById('path3038-1'), 'cross');
 
-    this.dpadLeftShape = svgDoc.getElementById('rect4086-17');
-    this.dpadRightShape = svgDoc.getElementById('rect4086-1');
-    this.dpadUpShape = svgDoc.getElementById('rect4086-6');
-    this.dpadDownShape = svgDoc.getElementById('rect4086');
+    this.makeButtonInteractive(this.dpadLeftShape = svgDoc.getElementById('rect4086-17'), 'dpadLeft');
+    this.makeButtonInteractive(this.dpadRightShape = svgDoc.getElementById('rect4086-1'), 'dpadRight');
+    this.makeButtonInteractive(this.dpadUpShape = svgDoc.getElementById('rect4086-6'), 'dpadUp');
+    this.makeButtonInteractive(this.dpadDownShape = svgDoc.getElementById('rect4086'), 'dpadDown');
 
-    this.rightStickShape = svgDoc.getElementById('path4031-7');
-    this.leftStickShape = svgDoc.getElementById('path4031');
+    this.makeButtonInteractive(this.l1Shape = svgDoc.getElementById('rect4250'), 'l1');
+    this.makeButtonInteractive(this.r1Shape = svgDoc.getElementById('rect4250-9'), 'r1');
 
-    this.l1Shape = svgDoc.getElementById('rect4250');
-    this.r1Shape = svgDoc.getElementById('rect4250-9');   
+    this.makeButtonInteractive(this.shareShape = svgDoc.getElementById('path2995'), 'share');
+    this.makeButtonInteractive(this.optionsShape = svgDoc.getElementById('path2995-7'), 'options');
+    
+    this.makeButtonInteractive(this.leftStickShape = svgDoc.getElementById('path4031'), 'l3');
+    this.makeButtonInteractive(this.rightStickShape = svgDoc.getElementById('path4031-7'), 'r3');    
 
     this.l3Shape = svgDoc.getElementById('path4166-3-4-13-7');
-    this.r3Shape = svgDoc.getElementById('path4166-3-4-13-7-1');    
-
-    this.shareShape = svgDoc.getElementById('path2995');
-    this.optionsShape = svgDoc.getElementById('path2995-7');
+    this.r3Shape = svgDoc.getElementById('path4166-3-4-13-7-1');
 
     const bodyShell = svgDoc.getElementById('g3324')
 
@@ -197,6 +228,26 @@ class PSController extends HTMLElement {
     bodyShell.appendChild(this.rTrigger);
     this.bringToTopofSVG(this.lTrigger);
     this.bringToTopofSVG(this.rTrigger);
+    
+    this.ds4svg.isSetUp = true;
+  }
+  
+  makeButtonInteractive(element, state) {
+    element.onpointerover = () => { 
+      element.style.opacity = 0.5; 
+    };     
+    element.onpointerout = () => { 
+      this.userState.buttons[state] = false;
+      element.style.opacity = 1;
+    };   
+    element.onpointerdown = () => {
+      this.userState.buttons[state] = true;
+      element.style.opacity = 1;
+    };
+    element.onpointerup = () => {
+      this.userState.buttons[state] = false;
+      element.style.opacity = 0.5; 
+    };
   }
 
   makeCircle(x, y, r, fill, stroke, opacity) {
@@ -221,9 +272,11 @@ class PSController extends HTMLElement {
     this.gzLine = svgDoc.getElementById('force_z');
     svgDoc.getElementById('text_title').textContent='Gyroscope';
 
-    const svgElement = svgDoc.getElementById(2)
+    const svgElement = svgDoc.getElementById('main');
     this.ds4svg.contentDocument.getElementById('g3324').appendChild(svgElement);
     svgElement.setAttribute('transform', `translate(150, 260) scale(0.5)`);
+    
+    this.gyroSvg.isSetUp = true;
   }
 
   setupAccelSVG() {
@@ -233,9 +286,57 @@ class PSController extends HTMLElement {
     this.azLine = svgDoc.getElementById('force_z');
     svgDoc.getElementById('text_title').textContent='Accelerometer';
 
-    const svgElement = svgDoc.getElementById(2)
+    const svgElement = svgDoc.getElementById('main');
     this.ds4svg.contentDocument.getElementById('g3324').appendChild(svgElement);
     svgElement.setAttribute('transform', `translate(300, 260) scale(0.5)`);
+    
+    this.accelSvg.isSetUp = true;
+  }
+  
+  getIinitalState() {
+    return {
+      interface: 'usb',
+      axes: {
+          leftStickX: 128,
+          leftStickY: 128,
+          rightStickX: 128,
+          rightStickY: 128,
+          l2: 0,
+          r2: 0,
+          gyroX: 30000,
+          gyroY: 30000,
+          gyroZ: 30000,
+          accelX: -15000,
+          accelY: -15000,
+          accelZ: -15000
+      },
+      buttons: {
+          triangle: false,
+          circle: false,
+          cross: false,
+          square: false,
+          dPadUp: false,
+          dPadRight: false,
+          dPadDown: false,
+          dPadLeft: false,
+          l1: false,
+          r1: false,
+          l2: false,
+          r2: false,
+          share: false,
+          options: false,
+          l3: false,
+          r3: false,
+          playStation: false,
+          touchPadClick: false
+      },
+      touchpad: {
+          touches: []
+      },
+      timestamp: 99999,
+      charging: false,
+      battery: 100
+    };
   }
 }
 customElements.define('ps-controller', PSController);
